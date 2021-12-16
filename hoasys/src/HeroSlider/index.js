cfdbddb4e4b98f8b1ec9a81602d9dedadb13fce3
Angelo Niel I. Navarro <espren.anjo101@gmail.com>
2021-12-16 17:36:55 +0800

    HeroSlider pic and slideshow

--- hoasys/src/HeroSlider/index.js
+++ hoasys/src/HeroSlider/index.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 
-function HeroSlider(props) {
+function HeroSlider({imgArr}) {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <p>You clicked {count} times.</p>
-            <button onClick={() => setCount(count - 1)}>
+        <div className='relative'>
+            <button className='opacity-0 float-left h-full inline-block absolute top-0 left-0' onClick={() => (count < 1) ? setCount(imgArr.length - 1) : setCount(count - 1)}>
                 Previous
             </button>
-            <img src='props.imgArr'/>
-            <button onClick={() => setCount(count + 1)}>
+            <img src={imgArr[count]}/>
+            <button className='opacity-0 float-right h-full inline-block absolute top-0 right-0' onClick={() => (count < (imgArr.length - 1)) ? setCount(count + 1) : setCount(0)}>
                 Next
             </button>
+            <div className=''>
+                {imgArr.map((item, index) => <span key={index} className={(index === count ? 'bg-black ' : null) + 'rounded-sm inline-block w-4 h-2 border'} />)}
+            </div>
         </div>
     );
 }
--- hoasys/src/pages/Home.js
+++ hoasys/src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <main>
-        <HeroSlider imgArr={['https://picsum.photos/200/300', 'https://picsum.photos/id/237/200/300']} />
+        <HeroSlider imgArr={['https://res.cloudinary.com/demo/image/upload/sample.gif', 'https://res.cloudinary.com/demo/basketball_shot.jpg', 'https://www.researchgate.net/profile/Elif-Bayramoglu/publication/322918596/figure/fig3/AS:669304651530259@1536586072864/Sample-example-of-xeriscape-URL-3.jpg']} />
         <p>This is the first landing page of the System.</p>
       </main>
     </>
