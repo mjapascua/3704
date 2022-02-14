77ace3a0b5517461a2c686fc5c91cc7465f1cfb8
Angelo Niel I. Navarro <espren.anjo101@gmail.com>
2022-02-14 14:11:03 +0800

    user, pass, and forgot
    basic html lang and css.. my guide is here:
    
    https://www.codingnepalweb.com/popup-login-form-design-html-css/

--- hoasys/src/components/Login/Login.js
+++ hoasys/src/components/Login/Login.js
@@ -2,7 +2,29 @@ function Login() {
     return (
         <>
             <main>
-                
+                <body>
+                    <span>
+                        <div class="container">
+                            <label for="show" class="close-btn fas fa-times" title="close"></label>
+                            <div class="text">
+                                Login Form
+                            </div>
+                            <form action="#">
+                                <div class="data">
+                                    <label>Email or Phone</label>
+                                    <input type="text" required />
+                                </div>
+                                <div class="data">
+                                    <label>Password</label>
+                                    <input type="password" required />
+                                </div>
+                                <div class="forgot-pass">
+                                    <a href="#">Forgot Password?</a>
+                                </div>
+                            </form>
+                        </div>
+                    </span>
+                </body>
             </main>
         </>
     );
