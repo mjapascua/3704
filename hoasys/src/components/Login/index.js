fba01e143176c7000f2126cd9f8b7925c0a7c39f
Angelo Niel I. Navarro <espren.anjo101@gmail.com>
2022-02-15 11:03:17 +0800

    Login and signup page css

--- hoasys/src/components/Login/index.js
+++ hoasys/src/components/Login/index.js
@@ -4,29 +4,48 @@ function Login() {
             <main>
                 <body>
                     <span>
-                        <div className="container">
-                            <label for="show" className="close-btn fas fa-times" title="close"></label>
-                            <div className="text">
+                        <div
+                            class="container"
+                            className="p-2
+                             bg-meadow-700
+                              w-60
+                              h-500
+                              mb-24
+                              justify-center
+                              mx-auto 
+                              flex 
+                              flex-col
+                              text-white"
+                        >
+                            <label for="show" class="close-btn fas fa-times" title="close"></label>
+                            <div 
+                                class="text"
+                                className="p-2
+                                 inline-block"
+                            >
                                 Login Form
                             </div>
                             <form action="#">
-                                <div className="data">
+                                <div class="data">
                                     <label>Email or Phone</label>
                                     <input type="text" required />
                                 </div>
-                                <div className="data">
+                                <div class="data">
                                     <label>Password</label>
                                     <input type="password" required />
                                 </div>
-                                <div className="forgot-pass">
+                                <div class="forgot-pass">
                                     <a href="#">Forgot Password?</a>
                                 </div>
-                                <div className="btn">
-                                    <div className="inner"></div>
+                                <div
+                                    class="btn"
+                                    className="bg-meadow-900"
+                                >
+                                    <div class="inner"></div>
                                     <button type="submit">LOGIN</button>
                                 </div>
-                                <div className="signup-link">
-                                    Not a member? <a href="#">Signup now.</a>
+                                <div class="signup-link">
+                                    Not a member? <a class="underline" href="#">Signup now.</a>
                                 </div>
                             </form>
                         </div>
--- hoasys/src/components/Signup/index.js
+++ hoasys/src/components/Signup/index.js
@@ -4,33 +4,52 @@ function Login() {
             <main>
                 <body>
                     <span>
-                        <div className="container">
-                            <label for="show" className="close-btn fas fa-times" title="close"></label>
-                            <div className="text">
+                        <div
+                            class="container"
+                            className="p-2
+                            bg-salmon-600
+                              w-60
+                              h-500
+                              mb-24
+                              justify-center
+                              mx-auto 
+                              flex 
+                              flex-col
+                              text-white"
+                        >
+                            <label for="show" class="close-btn fas fa-times" title="close"></label>
+                            <div
+                                class="text"
+                                className="p-2
+                                 inline-block"
+                            >
                                 Signup Form
                             </div>
                             <form action="#">
-                                <div className="data">
+                                <div class="data">
                                     <label>Email</label>
                                     <input type="text" required />
                                 </div>
-                                <div className="data">
+                                <div class="data">
                                     <label>Phone</label>
                                     <input type="text" required />
                                 </div>
-                                <div className="data">
+                                <div class="data">
                                     <label>Password</label>
                                     <input type="password" required />
                                 </div>
-                                <div className="forgot-pass">
+                                <div class="forgot-pass">
                                     <a href="#">Forgot Password?</a>
                                 </div>
-                                <div className="btn">
-                                    <div className="inner"></div>
-                                    <button type="submit">LOGIN</button>
+                                <div
+                                    class="btn"
+                                    className="bg-salmon-800"
+                                >
+                                    <div class="inner"></div>
+                                    <button type="submit">SIGNUP</button>
                                 </div>
-                                <div className="signup-link">
-                                    Not a member? <a href="#">Signup now.</a>
+                                <div class="signup-link">
+                                    Already a member? <a className="underline" href="#">Log-in.</a>
                                 </div>
                             </form>
                         </div>
