b11c3c6a8eb570d87b17204ddcacf41ee90e5f90
Angelo Niel I. Navarro <espren.anjo101@gmail.com>
2022-02-20 11:18:36 +0800

    HOA user controller and routes setup finish

--- backend/controllers/hoauserController.js
+++ backend/controllers/hoauserController.js
@@ -1,30 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get HOA users
 // @route   GET /api/hoausers
 // @access  Private
-const getHOAusers = (req, res) => {
+const getHOAusers = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get HOA users" })
-}
+})
 
 // @desc    Set HOA users
 // @route   POST /api/hoausers
 // @access  Private
-const setHOAuser = (req, res) => {
+const setHOAuser = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please enter an email')
+    }
+    
     res.status(200).json({ message: "Set HOA user" })
-}
+})
 
 // @desc    Update HOA user
 // @route   PUT /api/hoausers/:id
 // @access  Private
-const updateHOAuser = (req, res) => {
+const updateHOAuser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update HOA user ${req.params.id}` })
-}
+})
 
 // @desc    Delete HOA user
 // @route   DELETE /api/hoausers/:id
 // @access  Private
-const deleteHOAuser = (req, res) => {
+const deleteHOAuser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete HOA user ${req.params.id}` })
-}
+})
 
 module.exports = {
     getHOAusers,
