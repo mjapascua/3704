8badbad88ded92fbf98dc7d7362d234dfd96b905
Angelo Niel I. Navarro <espren.anjo101@gmail.com>
2022-02-21 20:01:06 +0800

    Backend Authentication through JWT
    finally not clunky.. ganto yata gumagamit ng NFTs, is it?

---
+++ backend_dir/backend/models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: [true, 'Please add your given name']
+    },
+
+    last_name: {
+      type: String,
+      required: [true, 'Please add your surname']
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+
+    phone_number: {
+      type: String,
+      match: /^[0-9]*$/,
+      minLength: 10,
+      maxLength: 10,
+      required: [true, 'Please add a phone number'],
+      unique: true
+    },
+
+    residence: {
+      type: String,
+      required: [true, 'Please add your residence']
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Please add a password']
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
