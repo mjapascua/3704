1fbf71f46b7dfba4d0c5c3ce4cca072ffc5b2c08
Meph27 <61321282+Meph27@users.noreply.github.com>
2022-03-05 07:19:35 +0800

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -178,13 +178,21 @@ export const Sidemenu = ({ style }) => {
       })}
       <span
         onClick={() => {
-          dispatch(logout());
-          navigate("/", { replace: true });
+          navigate("/account", { replace: true });
         }}
         className={defStyle}
       >
         <span className="material-icons-sharp md:mr-5">logout</span>
         <b className="md:w-max md:block hidden">Logout</b>
+      </span>
+      <span
+        onClick={() => {
+          navigate("/account", { replace: true });
+        }}
+        className={defStyle}
+      >
+        <span className="material-icons-sharp md:mr-5">face</span>
+        <b className="md:w-max md:block hidden">User page</b>
       </span>
     </div>
   );
