b04148a682ef4df1956f30c4184242578126aafd
Meph27 <61321282+Meph27@users.noreply.github.com>
2022-07-04 12:44:09 +0800

    Update Main.jsx

--- client/src/components/Buttons/Main.jsx
+++ client/src/components/Buttons/Main.jsx
@@ -8,7 +8,7 @@ export const Button = React.forwardRef((props, ref) => {
       onClick={props.onClick}
       className={
         props.className +
-        " font-display box-border select-none py-2 items-center justify-center font-semibold text-sm rounded-sm disabled:bg-gray-300 disabled:text-gray-700 disabled:border-gray-300" +
+        " font-display box-border select-none py-2 items-center justify-center font-semibold text-sm rounded-sm disabled:!bg-gray-300 disabled:text-gray-700 disabled:!border-gray-300" +
         (props.primary && primaryClass) +
         (props.secondary && secondaryClass)
       }
